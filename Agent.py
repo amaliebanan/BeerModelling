e83c9d65f2304cb0ed5455671b21d38c627c00ed
amaliebanan <qtb931@alumni.ku.dk>
2021-03-26 10:33:48 +0100

    Working

--- Agent.py
+++ Agent.py
@@ -17,19 +17,21 @@ def wander(self):
 def dispatch_time():             # genererer random integer mellem 1 og 12 der følger normalfordeling med mean 4 std deviation 2.
     lower, upper = 1, 12         #min 10 sek max 2 min
     mu, sigma = 4, 2               #mean 4 std deviation 2
-    return int(truncnorm((lower - mu) /sigma, (upper - mu) /sigma, loc = mu, scale=sigma)) #returnerer trunctuated normal distribution random variabel as int
+    return math.floor(truncnorm.rvs((lower - mu) /sigma, (upper - mu) /sigma, loc = mu, scale=sigma)) #returnerer trunctuated normal distribution random variabel as int
 
 def buy_beer(self):
     correct_employee = [a for a in self.model.grid.get_neighbors(self.pos,moore=True,include_center=False,radius=1) if isinstance(a,employee)][0]
     #correct_employee.dispatch_time = 1 #hvad er dispatch_time?
     self.employer = correct_employee
 
+    number_beers_bought = 2
+
     if correct_employee.busy is False:
         correct_employee.busy = True
         correct_employee.dispatch_time = dispatch_time()
         self.buying_beer_counter = correct_employee.dispatch_time
         self.beers_bought = random.randint(1,8) #køber mellem 1 og 8 øl
-        self.stall.beers_ready = self.stall.beers_ready - self.number_beers_bought #trækker fra antall øl købt
+        correct_employee.stall.beers_ready = correct_employee.stall.beers_ready - number_beers_bought #trækker fra antall øl købt
 
     #if correct_employee.busy is True:
         #queue() - skriv funktion er sætter gæsten i kø.
